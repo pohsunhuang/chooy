ff04e4af185f443bb755ebb22c77dd265e1e704d
pohsunhuang <pohsunhuang@chooy.com>
2017-11-12 10:35:03 +0800

    fix(FormChips): Vulcan smart form think the field is empty if unchanged. [why] Submit must gather data for every field, but FormChips only provide data on blur event. [how] Provide data at componentWillMount life cycle method

--- packages/chooy/lib/components/form/FormChips.js
+++ packages/chooy/lib/components/form/FormChips.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Chips from '../common/Chips';
 import { getI18nMessage } from '../../modules/utils';
 
-const FormChips = ({refFunction, value, label, options, ...props}) => {
-  return (
-    <div className="form-group row">
-      <label className="control-label col-sm-3">{label}</label>
-      <div className="col-sm-9">  
-        <Chips 
-          value=''
-          placeholder={getI18nMessage(options.placeholder)}
-          items={value} 
-          ref={refFunction} 
-          {...props} 
-        />
-      </div>  
-    </div>    
-  );
+class FormChips extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  /*
+
+  Add to autofilled values so SmartForms doesn't think the field is empty
+  if the user submits the form without changing it
+
+  */
+  componentWillMount() {
+    this.context.addToAutofilledValues({[this.props.name]: this.props.value || []});
+  }
+
+  render() {
+    const { refFunction, value, label, options, ...props } = this.props;
+
+    return (
+      <div className="form-group row">
+        <label className="control-label col-sm-3">{label}</label>
+        <div className="col-sm-9">  
+          <Chips
+            value=''
+            placeholder={getI18nMessage(options.placeholder)}
+            items={value}
+            ref={refFunction}
+            {...props}
+          />
+        </div>  
+      </div>      
+    );
+  }
 }
+
+FormChips.contextTypes = {
+  addToAutofilledValues: PropTypes.func,
+};
 
 export default FormChips;
