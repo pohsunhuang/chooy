d534dfefea4b5f7c3be9d64fa257820fef4c8bfc
pohsunhuang <pohsunhuang@chooy.com>
2017-12-08 00:07:00 +0800

    feat(Icon): Add Font Awesome support [how] 1. Enumerate all svg icon names       2. Return Font Awesome object if input name doesn't match with any of the svg names

--- packages/chooy/lib/components/common/Icons.js
+++ packages/chooy/lib/components/common/Icons.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
-const Icon = ({ name, size }) =>
-  <svg width={size} height={size}>
-    <use xlinkHref={`/packages/chooy/lib/assets/icons/icons.svg#icon-${name}`} />
-  </svg>
+const svgs = [
+  'chooy', 'prev', 'next',
+];
+
+const isSvg = name => svgs.indexOf(name) != -1;
+
+const Icon = ({ name, size, text, className }) => {
+  if (isSvg(name)) {
+    return (
+      <svg width={size} height={size}>
+        <use xlinkHref={`/packages/chooy/lib/assets/icons/icons.svg#icon-${name}`} />
+      </svg>
+    );
+  } else {
+    return (
+      <span className='fa-stack'>
+        <i className={`${className} fa fa-fw fa-${name} fa-stack-1x ${name == 'square' ? 'fa-lg' : ''}`} aria-hidden="true"/>
+        <span className='fa-stack-1x'>{text}</span>
+      </span>
+    );
+  }
+}
+
+
 
 export default Icon;  
