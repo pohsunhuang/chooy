a4b5056e41803a2a8aad52409ee57cb57c846854
pohsunhuang <pohsunhuang@chooy.com>
2017-12-10 14:52:38 +0800

    feat(Editor): Introduce new feature 'Link' for rich text editor.

--- packages/chooy/lib/components/common/Editor/Editor.js
+++ packages/chooy/lib/components/common/Editor/Editor.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Components } from 'meteor/vulcan:core';
 import { Editor as SlateEditor } from 'slate-react';
 import { Value } from 'slate';
 
-import Icon from './Icons';
+import EditorLinkModal from './EditorLinkModal';
+import Icon from '../Icons';
 
 const DEFAULT_NODE = 'paragraph'
 
 class Editor extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showLinkModal: false,
+      linkModalUrlOnly: false,
+    }
   }
 
   static propTypes = {
@@ -38,6 +45,61 @@ class Editor extends Component {
           ]
         }
     });
+  }
+
+  wrapLink = (change, href) => {
+    change.wrapInline({
+      type: 'link',
+      data: { href },
+    });
+  
+    change.collapseToEnd();
+  }
+  
+  unwrapLink = (change) => {
+    change.unwrapInline('link');
+  }
+
+  hasLinks = () => {
+    return this.props.value.inlines.some(inline => inline.type == 'link');
+  }
+
+  onShowLinkModal = (linkModalUrlOnly) => {
+    this.setState(state => ({showLinkModal: true, linkModalUrlOnly}));
+  }
+
+  onHideLinkModal = () => {
+    this.setState(state => ({showLinkModal: false}))
+  }
+
+  onSubmitLink = (url, text) => {
+    const change = this.props.value.change();
+
+    if (this.props.value.isExpanded) {
+      change.call(this.wrapLink, url);
+    } else {
+      change.insertText(text)
+            .extend(0 - text.length)
+            .call(this.wrapLink, url);      
+    }
+
+    this.props.onChange(change);
+    this.onHideLinkModal();
+  }
+
+  onClickLink = (e) => {
+    e.preventDefault();
+    const change = this.props.value.change();
+
+    if (this.hasLinks()) {
+      change.call(this.unwrapLink);
+    } else if (this.props.value.isExpanded) {
+      this.onShowLinkModal(true);
+    } else {
+      this.onShowLinkModal(false);
+    }
+
+    this.props.onChange(change);
   }
 
   hasMark = (type) => {
@@ -99,7 +161,7 @@ class Editor extends Component {
       <span key={idx} className="editor-button" onMouseDown={onMouseDown} onClick={onClick}>
         <Icon className={`editor-button-icon${isActive ? ' active' : ''}`} name={icon}/>
       </span>
-    )
+    );
   }
 
   renderBlockButton = ({type, icon}, idx) => {
@@ -111,7 +173,24 @@ class Editor extends Component {
       <span key={idx} className="editor-button" onMouseDown={onMouseDown} onClick={onClick}>
         <Icon className={`editor-button-icon${isActive ? ' active' : ''}`} name={icon} text={type == 'heading-one' ? '1' : (type == 'heading-two' ? '2' : '')}/>
       </span>
-    )
+    );
+  }
+
+  renderLinkButton = () => {
+    const isActive = this.hasLinks();
+    const onMouseDown = e => e.preventDefault();
+
+    return (
+      <span className='editor-button' onMouseDown={onMouseDown} onClick={this.onClickLink}>
+        <Icon className={`editor-button-icon${isActive ? ' active' : ''}`} name='link'/>
+        <EditorLinkModal
+          show={this.state.showLinkModal}
+          onHide={this.onHideLinkModal}
+          onSubmit={this.onSubmitLink}
+          urlOnly={this.state.linkModalUrlOnly}
+        />
+      </span>
+    );
   }
 
   renderEditorToolBar = () => {
@@ -134,6 +213,7 @@ class Editor extends Component {
       <div className="editor-toolbar">
         {markButtons.map((markButton, idx) => this.renderMarkButton(markButton, idx))}
         {blockButtons.map((blockButton, idx) => this.renderBlockButton(blockButton, idx))}
+        {this.renderLinkButton()}
       </div>
     )
   }
@@ -147,6 +227,7 @@ class Editor extends Component {
       case 'heading-two': return <h2 {...attributes}>{children}</h2>
       case 'list-item': return <li {...attributes}>{children}</li>
       case 'numbered-list': return <ol {...attributes}>{children}</ol>
+      case 'link': return <a {...attributes} href={node.data.get('href')}>{children}</a>
     }
   }
 
---
+++ packages/chooy/lib/components/common/Editor/EditorLinkModal.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-bootstrap/lib/Modal';
+
+class EditorLinkModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      url: '',
+      text: '',
+    }
+  }
+
+  static propTypes = {
+    show: PropTypes.bool.isRequired,
+    onHide: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    urlOnly: false,
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.show && !this.props.show) {
+      this.setState(state => ({url: '', text: '',}));
+    }
+  }
+
+  onChangeURL = (e) => {
+    const url = e.target.value;
+    this.setState(state => ({url}));
+  }
+
+  onChangeText = (e) => {
+    const text = e.target.value;
+    this.setState(state => ({text}));
+  }
+
+  onClickButton = (e) => {
+    e.preventDefault();
+
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.state.url, this.state.text);
+    }
+  }
+
+  render() {
+    return (
+      <Modal
+        className={this.props.className}
+        show={this.props.show}
+        onHide={this.props.onHide}
+        dialogClassName={this.props.dialogClassName}      
+      >
+        <Modal.Header className='editor-modal-header' closeButton>
+          <Modal.Title>Add Link</Modal.Title>
+        </Modal.Header>  
+        <Modal.Body>
+          <div className='editor-modal-body'>
+            <input placeholder='Enter link URL' value={this.state.url} onChange={this.onChangeURL}/>
+            {!this.props.urlOnly ? <input placeholder='Enter text' value={this.state.text} onChange={this.onChangeText}/> : null}
+            <button onClick={this.onClickButton}>OK</button>
+          </div>
+        </Modal.Body>        
+      </Modal>
+    );
+  }
+}
+
+export default EditorLinkModal;
