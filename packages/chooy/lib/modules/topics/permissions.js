79761e03f6e68653a8eb3b3c7d24c0bd9ab924de
pohsunhuang <pohsunhuang@chooy.com>
2018-03-15 16:27:52 +0800

    fix(TopicsEditForm): Users can't edit topic they created.
    [why]
    
    Permission check within Vulcan default_mutation is buggy, specify edit.all doesn't grant user the right to edit.own.
    
    [how]
    
    Accept edit.all, we need to declare the right of edit.own as well.

--- packages/chooy/lib/modules/topics/permissions.js
+++ packages/chooy/lib/modules/topics/permissions.js
@@ -2,6 +2,7 @@ import Users from 'meteor/vulcan:users';
 
 Users.groups.members.can([
   'topics.new',
+  'topics.edit.own', // the permission check within default_mutation is buggy, specify edit.all doesn't grant you the right to edit.own
   'topics.edit.all',
   'topics.remove.own',
 ]);
